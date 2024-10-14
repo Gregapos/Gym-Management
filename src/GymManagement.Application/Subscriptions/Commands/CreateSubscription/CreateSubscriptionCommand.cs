720bb540dddc048756fb1ba6e39340e636a62c6b
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-10-14 23:01:02 +0300

    implement CQRS and Mediator patterns

---
+++ src/GymManagement.Application/Subscriptions/Commands/CreateSubscription/CreateSubscriptionCommand.cs
@@ -0,0 +1,8 @@
+using MediatR;
+
+namespace GymManagement.Application.Subscriptions.Commands.CreateSubscriptionCommand;
+
+public record CreateSubscriptionCommand(
+   string SubscriptionType,
+   Guid AdminId
+) : IRequest<Guid>;
