e97681b94ef91b96826ba5b1fd48d38f11646a09
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-10-25 21:57:04 +0300

    implement error handling

--- src/GymManagement.Api/Controllers/SubscriptionsController.cs
+++ src/GymManagement.Api/Controllers/SubscriptionsController.cs
@@ -11,7 +11,7 @@ namespace GymManagement.Api.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class SubscriptionsController : ControllerBase
+public class SubscriptionsController : ApiController
 {
     private readonly ISender _mediator;
 
@@ -44,7 +44,7 @@ public class SubscriptionsController : ControllerBase
                 new SubscriptionResponse(
                     subscription.Id,
                     ToDto(subscription.SubscriptionType))),
-            error => Problem());
+            Problem);
     }
 
     [HttpGet("{subscriptionId:guid}")]
@@ -58,7 +58,7 @@ public class SubscriptionsController : ControllerBase
             subscription => Ok(new SubscriptionResponse(
                 subscription.Id,
                 ToDto(subscription.SubscriptionType))),
-            error => Problem());
+            Problem);
     }
 
     [HttpDelete("{subscriptionId:guid}")]
@@ -70,7 +70,7 @@ public class SubscriptionsController : ControllerBase
 
         return createSubscriptionResult.Match<IActionResult>(
             _ => NoContent(),
-            _ => Problem());
+            Problem);
     }
 
     private static SubscriptionType ToDto(DomainSubscriptionType subscriptionType)
--- src/GymManagement.Api/Program.cs
+++ src/GymManagement.Api/Program.cs
@@ -2,30 +2,30 @@ using GymManagement.Application;
 using GymManagement.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddControllers();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    builder.Services.AddProblemDetails();
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-builder.Services.
-    AddApplication()
-    .AddInfrastructure();
+    builder.Services.
+        AddApplication()
+        .AddInfrastructure();
+}
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseExceptionHandler();
+
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
