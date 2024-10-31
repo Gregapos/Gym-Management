894d842f41156dc1cf3a91c990082c0d3049a3fa
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-10-31 21:21:36 +0200

    implement application layer cross-cutting concerns via open generic PipelineBehaviors

--- src/GymManagement.Application/DependencyInjection.cs
+++ src/GymManagement.Application/DependencyInjection.cs
@@ -1,4 +1,6 @@
 using ErrorOr;
+using FluentValidation;
+using GymManagement.Application.Common.Behaviors;
 using GymManagement.Application.Gyms.Commands.CreateGym;
 using GymManagement.Domain.Gyms;
 using MediatR;
@@ -13,10 +15,10 @@ public static class DependencyInjection
         services.AddMediatR(options =>
         {
             options.RegisterServicesFromAssemblyContaining(typeof(DependencyInjection));
-            options.AddBehavior<
-                IPipelineBehavior<CreateGymCommand, ErrorOr<Gym>>,
-                CreateGymCommandBehavior>();
+            options.AddOpenBehavior(typeof(ValidationBehavior<,>));
         });
+
+        services.AddValidatorsFromAssemblyContaining(typeof(DependencyInjection));
 
         return services;
     }
