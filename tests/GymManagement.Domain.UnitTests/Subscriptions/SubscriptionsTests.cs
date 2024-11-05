4ebaa0c77a4956481071512e839818b31cf3a01f
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-11-05 21:10:47 +0200

    implement domain layer unit tests

---
+++ tests/GymManagement.Domain.UnitTests/Subscriptions/SubscriptionsTests.cs
@@ -0,0 +1,36 @@
+using ErrorOr;
+using FluentAssertions;
+using GymManagement.Domain.Subscriptions;
+using TestCommon.Gyms;
+using TestCommon.Subscriptions;
+
+namespace GymManagement.Domain.UnitTests.Subscriptions;
+
+public class SubscriptionsTests
+{
+    [Fact]
+    public void AddGymWhenMoreThanSubscriptionAllows_ShouldFail()
+    {
+        // Arrange
+        // Create a subscription
+        var subscription = SubscriptionFactory.CreateSubscription();
+
+        // Create the maximum number of gyms + 1
+        var gyms = Enumerable.Range(0, subscription.GetMaxGyms() + 1)
+            .Select(_ => GymFactory.CreateGym(id: Guid.NewGuid()))
+            .ToList();
+
+        // Act
+        // Add all various gyms
+        var addGymResults = gyms.ConvertAll(subscription.AddGym);
+
+        // Assert
+        // Adding all the gyms succeeded, but the last failed
+        var allButLastGymResults = addGymResults[..^1];
+        allButLastGymResults.Should().AllSatisfy(addGymResult => addGymResult.Value.Should().Be(Result.Success));
+
+        var lastAddGymResult = addGymResults.Last();
+        lastAddGymResult.IsError.Should().BeTrue();
+        lastAddGymResult.FirstError.Should().Be(SubscriptionErrors.CannotHaveMoreGymsThanSubscriptionAllows);
+    }
+}
