ddabfcec8b811d7c873b240b59a8e9e6b4b08934
Gregapos <34349683+Gregapos@users.noreply.github.com>
2024-11-14 15:02:28 +0200

    update application subcutaneous tests mediatorFactory

--- tests/GymManagement.Application.SubcutaneousTests/Common/MediatorFactory.cs
+++ tests/GymManagement.Application.SubcutaneousTests/Common/MediatorFactory.cs
@@ -1,4 +1,6 @@
 using GymManagement.Api;
+using GymManagement.Application.Common.Interfaces;
+using GymManagement.Application.Common.Models;
 using GymManagement.Infrastructure.Common.Persistence;
 using MediatR;
 using Microsoft.AspNetCore.Hosting;
@@ -7,12 +9,24 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using NSubstitute;
 
 namespace GymManagement.Application.SubcutaneousTests.Common;
 
 public class MediatorFactory : WebApplicationFactory<IAssemblyMarker>, IAsyncLifetime
 {
     private SqliteTestDatabase _testDatabase = null!;
+    private readonly ICurrentUserProvider _customUserProvider;
+
+    public MediatorFactory()
+    {
+        _customUserProvider = Substitute.For<ICurrentUserProvider>();
+        _customUserProvider.GetCurrentUser().Returns(new CurrentUser(
+            Guid.NewGuid(),
+            new List<string>(),
+            new List<string> { "Admin" }
+        ));
+    }
 
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
@@ -23,6 +37,10 @@ public class MediatorFactory : WebApplicationFactory<IAssemblyMarker>, IAsyncLif
             services
                 .RemoveAll<DbContextOptions<GymManagementDbContext>>()
                 .AddDbContext<GymManagementDbContext>((sp, options) => options.UseSqlite(_testDatabase.Connection));
+
+            services
+                .RemoveAll<ICurrentUserProvider>()
+                .AddSingleton(_customUserProvider);
         });
     }
 
